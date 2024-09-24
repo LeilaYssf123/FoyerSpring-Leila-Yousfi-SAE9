1a35f8c022e0083b4f098a34db1a75920c1bbb9b
leila <leila.yousfi@esprit.tn>
2024-09-24 10:21:24 +0100

    First commit

---
+++ src/main/java/tn/esprit/foyersae/FoyerSaeApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.foyersae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoyerSaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoyerSaeApplication.class, args);
+    }
+
+}
